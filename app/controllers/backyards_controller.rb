9307b29f124b2feddf99c8ed0074a71e0c93bd81
ryan erswell <ersie93@googlemail.com>
2021-02-22 17:56:48 +0000

    destroy method in backyardscontroller and view and route

--- app/controllers/backyards_controller.rb
+++ app/controllers/backyards_controller.rb
@@ -1,10 +1,22 @@
 class BackyardsController < ApplicationController
-  def show
-    @backyard = Backyard.find(params[:id])
-  end
+	before_action :set_backyard, only: [:show, :destroy]
+  
+  def show; end
 
 	def index
     @backyards = Backyard.all
 	end
+
+	def destroy
+		@backyard.destroy
+		redirect_to backyards_path
+	end
+
+	private
+
+	def set_backyard
+		@backyard = Backyard.find(params[:id])
+	end
+
 end
 
