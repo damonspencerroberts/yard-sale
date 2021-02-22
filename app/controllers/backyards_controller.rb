0da1546627f3f2a1c5eb050effbbf7434155bb2d
Damon Roberts <damonspencerroberts@gmail.com>
2021-02-22 17:49:35 +0000

    add new and create methods to backyards controller

--- app/controllers/backyards_controller.rb
+++ app/controllers/backyards_controller.rb
@@ -1,10 +1,28 @@
 class BackyardsController < ApplicationController
+  def index
+    @backyards = Backyard.all
+  end
+
   def show
     @backyard = Backyard.find(params[:id])
   end
 
-	def index
-    @backyards = Backyard.all
-	end
-end
+  def new
+    @backyard = Backyard.new
+  end
 
+  def create
+    @backyard = Backyard.new(backyard_params)
+    @backyard.user_id = current_user.id
+    if @backyard.save
+      redirect_to backyard_path(@backyard)
+    else
+      render :new
+    end
+  end
+
+  # must add photos below
+  def backyard_params
+    params.require(:backyard).permit(:name, :address, :price, :description, :max_capacity)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :backyards, only: [:index, :show]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :backyards, only: [:index, :show]
+  resources :backyards, only: [:index, :show, :new, :create]
 end
