4e2fde95afcc767809d653d1bf37637b748e3f43
ryan erswell <ersie93@googlemail.com>
2021-02-23 17:58:44 +0000

    removed require login

--- app/controllers/backyards_controller.rb
+++ app/controllers/backyards_controller.rb
@@ -37,11 +37,4 @@ class BackyardsController < ApplicationController
   def backyard_params
     params.require(:backyard).permit(:name, :address, :price, :description, :max_capacity)
   end
-
-  def require_login
-    unless logged_in?
-      flash[:error] = "You must be logged in to access this section"
-      redirect_to new_login_url # halts request cycle
-    end
-  end
 end
