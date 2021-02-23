593dec9ebf991ec282cb1681afcd731b38e11fe2
ryan erswell <ersie93@googlemail.com>
2021-02-23 13:25:13 +0000

    added bookings to seed and index and show to booking controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :backyards, only: [:index, :show, :new, :create]
+  resources :bookings
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require 'open-uri'
+require 'date'
 
 def open_file(file)
   URI.open(file)
@@ -233,6 +234,7 @@ backyards = [
   }
 ]
 
+
 backyards.each_with_index do |backyard, index|
   new_backyard = Backyard.new(
     name: backyard[:name],
@@ -247,4 +249,16 @@ backyards.each_with_index do |backyard, index|
     new_backyard.photos.attach(io: opened_photo, filename: "backyard#{index}-#{j}.jpg", content_type: 'image/jpg')
   end
   new_backyard.save
+end
+
+puts "cleaning database"
+puts "creating bookings"
+
+10.times do
+  Booking.create!(
+    date: Date.today + rand(1..100),
+    number_of_guests: rand(10..30),
+    user_id: User.all.ids.sample,
+    backyard_id: Backyard.all.ids.sample
+    )
 end
