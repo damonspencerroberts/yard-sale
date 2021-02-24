765eb3b8b55d6091066fcb07a8e6f3c6eed2ea2d
cosmina iosifescu <cosmina.iosifescu@gmail.com>
2021-02-24 13:09:46 +0100

    Finished implementing the geocode

--- app/models/backyard.rb
+++ app/models/backyard.rb
@@ -4,4 +4,6 @@ class Backyard < ApplicationRecord
   validates :name, :address, :price, :description, :max_capacity, presence: true
   validates :address, uniqueness: true
   has_many_attached :photos
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
