06f7535eec193c8e7b0e0443f05287dac8cfd4f1
cosmina iosifescu <cosmina.iosifescu@gmail.com>
2021-02-24 15:01:52 +0100

    Finished basic mapbox with markers. TODO:proper styling

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import { initMapbox } from "../plugins/init_mapbox";
 
 Rails.start()
 Turbolinks.start()
@@ -25,6 +24,7 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 import { initNavbarScroll } from '../components/init_navbar_scroll';
+import { initMapbox } from "../plugins/init_mapbox";
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
