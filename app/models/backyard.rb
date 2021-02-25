6d0a8099a7a9d3e9816cc6d926b9ae3ae8277f46
ryan erswell <ersie93@googlemail.com>
2021-02-25 12:23:32 +0000

    inplemented reviews routes

--- app/models/backyard.rb
+++ app/models/backyard.rb
@@ -1,6 +1,7 @@
 class Backyard < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
+  has_many :reviews, dependent: :destroy
   validates :name, :address, :price, :description, :max_capacity, presence: true
   validates :address, uniqueness: true
   has_many_attached :photos
