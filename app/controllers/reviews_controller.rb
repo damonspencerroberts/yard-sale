ba18cadc7d6a51c3e494d8dc5559dcfa0b930bbd
ryan erswell <ersie93@googlemail.com>
2021-02-25 15:36:56 +0000

    finished reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -12,8 +12,40 @@ class ReviewsController < ApplicationController
 		if @review.save!
 			redirect_to backyard_path(@backyard)
 		else
-			flash[:notice] = "Review not saved!"
+			flash.now[:notice] = "Review not saved!"
 			redirect_to backyard_path(@backyard)
+		end
+	end
+
+	def edit
+		@review = Review.find(params[:id])
+		if @review.user == current_user
+			@review = Review.find(params[:id])
+			@backyard = @review.backyard
+		else
+			flash.now[:notice] = "You cannot update this review"
+			redirect_to user_profile_reviews_path
+		end
+	end
+
+	def update
+		@review = Review.find(params[:id])
+		if @review.update(review_params)
+			redirect_to user_profile_reviews_path
+		else
+			flash.now[:notice] = "Cannot update this review"
+			redirect_to user_profile_reviews_path
+		end
+	end
+
+	def destroy
+		@review = Review.find(params[:id])
+		if @review.user == current_user
+			@review.destroy
+			redirect_to user_profile_reviews_path
+		else
+			flash.now[:notice] = "Cannot delete this review"
+			redirect_to user_profile_reviews_path
 		end
 	end
 
