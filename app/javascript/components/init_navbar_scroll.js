c855bbdcb06d6f2013e4e2b50ef740dc68ba3e8a
cosmina iosifescu <cosmina.iosifescu@gmail.com>
2021-02-25 16:11:10 +0100

    Finished the searchbar and added styling to index page

--- app/javascript/components/init_navbar_scroll.js
+++ app/javascript/components/init_navbar_scroll.js
@@ -4,7 +4,7 @@ const initNavbarScroll = () => {
 
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight) {
+      if (window.scrollY >= 10) {
          navbar.classList.add('navbar-lewagon-white');
       } else {
           navbar.classList.remove('navbar-lewagon-white');
