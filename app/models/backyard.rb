c99740ababcc9811a04bebd96a5ab7d65a1841f8
Damon Roberts <damonspencerroberts@gmail.com>
2021-02-25 11:52:46 +0000

    require user to upload image of backyard

--- app/models/backyard.rb
+++ app/models/backyard.rb
@@ -1,9 +1,10 @@
 class Backyard < ApplicationRecord
   belongs_to :user
+  has_many_attached :photos
   has_many :bookings, dependent: :destroy
   validates :name, :address, :price, :description, :max_capacity, presence: true
+  validates :photos, presence: true
   validates :address, uniqueness: true
-  has_many_attached :photos
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
