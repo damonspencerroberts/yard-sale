a351b2ffe36c40098aaac4f7f655d564e1e9335c
ryan erswell <ersie93@googlemail.com>
2021-02-25 16:55:51 +0000

    added stars to review

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,6 +23,7 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { initStarRating } from '../plugins/init_star_rating';
 import { initNavbarScroll } from '../components/init_navbar_scroll';
 import { initMapbox } from "../plugins/init_mapbox";
 
@@ -30,5 +31,10 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initNavbarScroll();
+  initStarRating();
   initMapbox();
+
 });
+
+
+
