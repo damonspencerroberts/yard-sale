fb6170f0dc9a3df3bfb71ea8aaaa6d982ed15b30
Damon Roberts <damonspencerroberts@gmail.com>
2021-02-26 13:55:21 +0000

    Edit styling of favourites

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -7,12 +7,13 @@ class FavouritesController < ApplicationController
     @backyard = Backyard.find(params[:backyard_id])
     @favourite = Favourite.new(user: current_user, backyard: @backyard)
     @favourite.save
-    redirect_to user_profile_favourites_path
+    redirect_to backyard_path(@backyard)
   end
 
   def destroy
+    @backyard = Backyard.find(params[:backyard_id])
     @favourite = Favourite.find(params[:id])
     @favourite.destroy
-    redirect_to user_profile_favourites_path
+    redirect_to backyard_path(@backyard)
   end
 end
